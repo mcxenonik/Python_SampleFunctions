efee9b0aff6f3728aebd362a13875b834b2dedf7
mcxenonik <01112955@pw.edu.pl>
2021-03-24 14:50:05 +0100

    Added word_to_dictionary.py

---
+++ word_to_dictionary.py
@@ -0,0 +1,20 @@
+"""
+Napisz funkcję, która jako parametr przyjmuje wyraz i tworzy słownik,
+gdzie kluczami są litery, zaś wartościami liczba ich wystąpień w danym
+słowie. Przykład: dla słowa "kukułka" funkcja powinna zwrócić:
+{'k': 3, 'u': 2, 'ł': 1, 'a': 1}
+"""
+
+
+def word_to_dict(word):
+    my_dict = {}
+    for character in word:
+        if character in my_dict:
+            my_dict[character] += 1
+        else:
+            my_dict[character] = 1
+
+    return my_dict
+
+
+print(word_to_dict('kukulka'))
